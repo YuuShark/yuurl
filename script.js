5fa1a75a0ff5e3aa60a3b03f917a1b92a4efc317
YuuShark <71966942+YuuShark@users.noreply.github.com>
2025-03-23 12:32:03 +0900

    Add files via upload
    動的処理用のjsとデザインのcssも追加

---
+++ script.js
@@ -0,0 +1,54 @@
+// メニュー
+document.addEventListener("DOMContentLoaded", function() {
+    const menu = document.getElementById('menu');
+    const menuIcon = document.querySelector('.menu-icon');
+    const menuLinks = document.querySelectorAll('.nav-menu a');
+        
+    if (localStorage.getItem('menuOpen') === 'true') {
+        menu.classList.add('show');
+    }
+        // メニューの表示、非表示の処理をする
+    function toggleMenu(event) {
+         event.stopPropagation();
+        // var menu = document.getElementById('menu');
+        menu.classList.toggle('show');
+
+        // メニューの状態を保存
+        localStorage.setItem('menuOpen', menu.classList.contains('show'));
+    }
+
+    // 画面外をクリックするとメニューが閉じる処理
+    document.addEventListener("click", function(event) {
+        if (!menu.contains(event.target) && !menuIcon.contains(event.target) && menu.classList.contains('show')) {
+            menu.classList.remove('show');
+            localStorage.setItem('menuOpen', 'false');
+        }
+    });
+
+    menuIcon.addEventListener("click", toggleMenu);
+
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    menuLinks.forEach(link => {
+        const href = link.getAttribute('href').replace('./', '');
+        if (currentPage === href) {
+            link.classList.add('active');
+        }
+        // リンククリック時にメニューを閉じないようにする処理
+        link.addEventListener('click', function(event) {
+            event.stopPropagation();
+        });
+    });
+});
+
+// 説明文の動的処理
+function toggleDescription(id) {
+    var descriptions = document.querySelectorAll(".description");
+
+    descriptions.forEach(desc => {
+        if (desc.id === id) {
+            desc.style.display = (desc.style.display === "block") ? "none" : "block";
+        } else {
+            desc.style.display = "none";
+        }
+    })
+}
